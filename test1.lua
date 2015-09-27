de465b4c7543085bd154d87224ed8e7f046b074e
Hugh Perkins <hughperkins@gmail.com>
2015-09-28 00:33:52 +0700

    handles 4d sparse, albeit hackily

--- test1.lua
+++ test1.lua
@@ -16,16 +16,21 @@
 
 local SparseTensor = torch.class('torch.SparseTensor')
 
-function torch.SparseTensor:__init(planes, rows, cols)
-   self.planes = planes
-   self.rows = rows
-   self.cols = cols
-   self.denseBySparse = {}
+function torch.SparseTensor:__init(size)
+   self.dims = size:size()
+   assert(self.dims >= 3)
+   self.size =  torch.LongStorage(self.dims)
+   self.size:copy(size)
+--   self.planes = planes
+   self.rows = size[self.dims-1]
+   self.cols = size[self.dims]
+   self.denseBySparse = {} -- if eg 4d, the lookup will be the linear position within the storage, if one suddenly shurnk the last two dimensions to 1, and 
+                           -- assuming close-packed contiguous (can probably phrase this better somehow)
    self.sparseByDense = {}
    self.planes = {}
 end
 
-function torch.SparseTensor:set(p, row, col, value)
+function torch.SparseTensor:set3d(p, row, col, value)
    local sparse = self.sparseByDense[p]
    if sparse == nil then
       sparse = #self.planes + 1
@@ -45,11 +50,33 @@ function torch.SparseTensor:get1d(p)
    return self.planes[sparse]
 end
 
-function torch.SparseTensor:addPlane(p, plane)
+function torch.SparseTensor:_pCoordToLinear(p_coord)
+   print('p_coord', p_coord)
+   local sparse_dims = p_coord:size()
+   print('sparse_dims', sparse_dims)
+   if sparse_dims == 1 then
+      return p_coord[1]
+   elseif sparse_dims == 2 then
+      return p_coord[1] * self.size[2] + p_coord[2]
+   else
+      error("not implemented")
+   end
+--   assert(p_coord:size() + 2 == self.size:size())
+--   local linear = 0
+--   for d=1,p_coord:size() do
+--      linear = linear * self.size[d]
+--      linear = linear + p_coord[d]
+--   end
+--   return linear
+end
+
+function torch.SparseTensor:addPlane(p_coord, plane)  -- p_coord is the cooridnates without hte last two cordinates, eg if this tensor is 4d, p_coord will be 2d
+   print('addPlane p_coord', p_coord, 'plane', plane)
+   local linearIndex = self:_pCoordToLinear(p_coord)
    local s = #self.planes + 1
    self.planes[s] = plane:clone()
-   self.denseBySparse[s] = p
-   self.sparseByDense[p] = s
+   self.denseBySparse[s] = linearIndex
+   self.sparseByDense[linearIndex] = s
    return self
 end
 
@@ -63,22 +90,139 @@ end
 
 function torch.SparseTensor:__tostring__()
    res = ''
-   for s, d in ipairs(self.denseBySparse) do
-      res = res .. 'feature plane ' .. d .. '\n'
-      res = res .. self.planes[s]:__tostring__()
+   local sparse_dims = self.size:size() - 2
+   if sparse_dims == 1 then
+      for s, d in ipairs(self.denseBySparse) do
+         res = res .. 'feature plane [' .. d .. ']\n'
+         res = res .. self.planes[s]:__tostring__()
+      end
+   elseif sparse_dims == 2 then
+      for s, dlinear in ipairs(self.denseBySparse) do
+         local dcoords = torch.LongStorage(2)
+         dcoords[1] = dlinear / self.size[2]
+         dcoords[2] = dlinear % self.size[2]
+         res = res .. 'feature plane [' .. dcoords[1] .. '][' .. dcoords[2] .. ']\n'
+         res = res .. self.planes[s]:__tostring__()
+      end
+   else
+      error("not implemented")
    end
    return res
 end
 
-function torch.SparseTensor.fromDense(dense, tolerance)
+--function torch.SparseTensor.fromDense(dense, tolerance)  -- if a plane has any value at least tolerance, then it's included, otherwise excluded
+--   tolerance = tolerance or 0.000001
+--   local dims = dense:dim()
+--   assert(dims >= 3)
+--   local sparse = torch.SparseTensor(dense:size())
+--   local sparse_dims = dense:size():size() - 2
+--   local sparse_coords = torch.LongStorage(sparse_dims)
+--   for d=1,sparse_dims do
+--      sparse_coords[d] = 1
+--   end
+--   local finished = false
+--   local dense_linear = 1
+--   local dense_by_d = {}
+--   dense_by_d[1] = dense[1]
+--   for d=2,sparse_dims do
+--      dense_by_d[d] = dense_by_d[d-1][1]
+--   end
+--   while not finished do
+--      print('fromDense sparse_dims', sparse_dims)
+--      if dense_by_d[sparse_dims]:clone():abs():max() >= tolerance then
+--         sparse:addPlane(sparse_coords,dense_by_d[sparse_dims])
+--      end
+--      local inced_d = sparse_dims
+--      sparse_coords[inced_d] = sparse_coords[inced_d] + 1
+--      if inced_d > 1 then
+--         dense_by_d[inced_d] = dense_by_d[inced_d - 1][inced_d]
+--      else
+--         dense_by_d[inced_d] = dense[inced_d]
+--      end
+--      while inced_d > 1 and sparse_coords[inced_d] > dense:size(inced_d) do
+--         sparse_coords[inced_d] = 1
+--         inced_d = inced_d - 1
+--         sparse_coords[inced_d] = sparse_coords[inced_d] + 1
+--      end
+--      if sparse_coords[inced_d] > dense:size(inced_d) then
+--         finished = true
+--      end
+--      dense_linear = dense_linear + 1
+--   end
+----   for d=1,dense:size(1) do
+----      if dense[d]:clone():abs():max() >= tolerance then
+----         sparse:addPlane(d,dense[d])
+----      end
+----   end
+--   return sparse
+--end
+
+function torch.SparseTensor.fromDense(dense, tolerance)  -- if a plane has any value at least tolerance, then it's included, otherwise excluded
    tolerance = tolerance or 0.000001
-   assert(dense:dim() == 3)
-   local sparse = torch.SparseTensor()
-   for d=1,dense:size(1) do
-      if dense[d]:clone():abs():max() >= tolerance then
-         sparse:addPlane(d,dense[d])
-      end
+   local sparse = torch.SparseTensor(dense:size())
+   local dims = dense:dim()
+   assert(dims >= 3)
+   if dims == 3 then
+      for d=1,dense:size(1) do
+         if dense[d]:clone():abs():max() >= tolerance then
+            sparse:addPlane(torch.LongStorage({d}),dense[d])
+         end
+      end      
+   elseif dims == 4 then
+      local denseLinear = 1
+      for d=1,dense:size(1) do
+         local dense1 = dense[d]
+         for e=1,dense:size(2) do
+            local dense2 = dense1[e]
+            if dense2:clone():abs():max() >= tolerance then
+               sparse:addPlane(torch.LongStorage({d,e}),dense2)
+            end
+            denseLinear = denseLinear + 1
+          end
+      end      
+   else
+      error("not implemented")
    end
+--   local sparse = torch.SparseTensor(dense:size())
+--   local sparse_dims = dense:size():size() - 2
+--   local sparse_coords = torch.LongStorage(sparse_dims)
+--   for d=1,sparse_dims do
+--      sparse_coords[d] = 1
+--   end
+--   local finished = false
+--   local dense_linear = 1
+--   local dense_by_d = {}
+--   dense_by_d[1] = dense[1]
+--   for d=2,sparse_dims do
+--      dense_by_d[d] = dense_by_d[d-1][1]
+--   end
+--   while not finished do
+--      print('fromDense sparse_dims', sparse_dims)
+--      if dense_by_d[sparse_dims]:clone():abs():max() >= tolerance then
+--         sparse:addPlane(sparse_coords,dense_by_d[sparse_dims])
+--      end
+--      local inced_d = sparse_dims
+--      sparse_coords[inced_d] = sparse_coords[inced_d] + 1
+--      if inced_d > 1 then
+--         dense_by_d[inced_d] = dense_by_d[inced_d - 1][inced_d]
+--      else
+--         dense_by_d[inced_d] = dense[inced_d]
+--      end
+--      while inced_d > 1 and sparse_coords[inced_d] > dense:size(inced_d) do
+--         sparse_coords[inced_d] = 1
+--         inced_d = inced_d - 1
+--         sparse_coords[inced_d] = sparse_coords[inced_d] + 1
+--      end
+--      if sparse_coords[inced_d] > dense:size(inced_d) then
+--         finished = true
+--      end
+--      dense_linear = dense_linear + 1
+--   end
+--   for d=1,dense:size(1) do
+--      if dense[d]:clone():abs():max() >= tolerance then
+--         sparse:addPlane(d,dense[d])
+--      end
+--   end
    return sparse
 end
 
@@ -102,8 +246,8 @@ function torch.SparseTensor:cmul(second)
    return self
 end
 
-local a = torch.SparseTensor(3,8,3)
-a:set(2,4,1, 4.5)
+local a = torch.SparseTensor(torch.LongStorage({3,8,3}))
+a:set3d(2,4,1, 4.5)
 print('a[2]', a:get1d(2))
 print('a[2][4][1]', a:get3d(2,4,1))
 
@@ -117,11 +261,13 @@ print('a[2][4][1]', a:get3d(2,4,1))
 print('a', a)
 
 -- add
-local b = torch.SparseTensor(3,8,3)
-b:set(2,4,1, 7.2)
+local b = torch.SparseTensor(torch.LongStorage({3,8,3}))
+b:set3d(2,4,1, 7.2)
 print('b', b)
 a:add(b)
 print('a', a)
+
+assert(b:_pCoordToLinear(torch.LongStorage({2})) == 2)
 
 -- multiply by element
 a:cmul(b)
@@ -165,6 +311,9 @@ print('5*5*3*3', 5*5*3*3)
 b_view = b:view(5,5,3,3)
 print('b_view:size()', b_view:size())
 b_sparse = torch.SparseTensor.fromDense(b_view)
+print('=========')
+print('b_view', b_view)
+print('=========')
 print('b_sparse', b_sparse)
 
 
